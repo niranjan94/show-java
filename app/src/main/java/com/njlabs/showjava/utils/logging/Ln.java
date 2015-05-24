0eb7571701bd741ba4602ee40a10dd874c019e19
Niranjan Rajendran <niranjan94@yahoo.com>
2015-05-24 16:17:08 +0530

    Added logger (Ln)

---
+++ app/src/main/java/com/njlabs/showjava/utils/logging/Ln.java
@@ -0,0 +1,96 @@
+package com.njlabs.showjava.utils.logging;
+
+/**
+ * Created by Niranjan on 12-07-2014.
+ */
+public final class Ln {
+    /**
+     * lnImpl is initially set to LnImpl() with sensible defaults, then replaced
+     * by whatever binding you choose during guice static injection pass.
+     */
+    @SuppressWarnings(value="MS_SHOULD_BE_FINAL")
+
+    protected static LnInterface lnImpl = new LnImpl();
+
+    private Ln() {}
+
+    public static int v(Throwable t) {
+        return lnImpl.v(t);
+    }
+
+    public static int v(Object s1, Object... args) {
+        return lnImpl.v(s1, args);
+    }
+
+    public static int v(Throwable throwable, Object s1, Object... args ) {
+        return lnImpl.v(throwable,s1,args);
+    }
+
+    public static int d(Throwable t) {
+        return lnImpl.d(t);
+    }
+
+    public static int d(Object s1, Object... args) {
+        return lnImpl.d(s1,args);
+    }
+
+    public static int d(Throwable throwable, Object s1, Object... args) {
+        return lnImpl.d(throwable, s1, args);
+    }
+
+    public static int i(Throwable t) {
+        return lnImpl.i(t);
+    }
+
+    public static int i( Object s1, Object... args) {
+        return lnImpl.i(s1, args);
+    }
+
+    public static int i(Throwable throwable, Object s1, Object... args) {
+        return lnImpl.i(throwable, s1, args);
+    }
+
+    public static int w(Throwable t) {
+        return lnImpl.w(t);
+    }
+
+    public static int w( Object s1, Object... args) {
+        return lnImpl.w(s1,args);
+    }
+
+    public static int w( Throwable throwable, Object s1, Object... args) {
+        return lnImpl.w(throwable,s1,args);
+    }
+
+    public static int e(Throwable t) {
+        return lnImpl.e(t);
+    }
+
+    public static int e( Object s1, Object... args) {
+        return lnImpl.e(s1,args);
+    }
+
+    public static int e( Throwable throwable, Object s1, Object... args) {
+        return lnImpl.e(throwable,s1,args);
+    }
+
+    public static boolean isDebugEnabled() {
+        return lnImpl.isDebugEnabled();
+    }
+
+    public static boolean isVerboseEnabled() {
+        return lnImpl.isVerboseEnabled();
+    }
+
+    public static int getLoggingLevel() {
+        return lnImpl.getLoggingLevel();
+    }
+
+    public static void setLoggingLevel(int level) {
+        lnImpl.setLoggingLevel(level);
+    }
+
+    public static String logLevelToString( int loglevel ) {
+        return lnImpl.logLevelToString(loglevel);
+    }
+}
---
+++ app/src/main/java/com/njlabs/showjava/utils/logging/LnInterface.java
@@ -0,0 +1,46 @@
+package com.njlabs.showjava.utils.logging;
+
+/**
+ * Created by Niranjan on 12-07-2014.
+ */
+public interface LnInterface {
+    int v(Throwable t);
+
+    int v(Object s1, Object... args);
+
+    int v(Throwable throwable, Object s1, Object[] args);
+
+    int d(Throwable t);
+
+    int d(Object s1, Object... args);
+
+    int d(Throwable throwable, Object s1, Object... args);
+
+    int i(Throwable t);
+
+    int i(Throwable throwable, Object s1, Object... args);
+
+    int i(Object s1, Object... args);
+
+    int w(Throwable t);
+
+    int w(Throwable throwable, Object s1, Object... args);
+
+    int w(Object s1, Object... args);
+
+    int e(Throwable t);
+
+    int e(Throwable throwable, Object s1, Object... args);
+
+    int e(Object s1, Object... args);
+
+    boolean isDebugEnabled();
+
+    boolean isVerboseEnabled();
+
+    int getLoggingLevel();
+
+    void setLoggingLevel(int level);
+
+    String logLevelToString(int loglevel);
+}
