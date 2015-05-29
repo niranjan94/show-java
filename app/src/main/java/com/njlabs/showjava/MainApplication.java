acc52251c9cf650bc4133c89716cd2b8670679ca
Niranjan Rajendran <niranjan94@yahoo.com>
2015-05-29 22:47:15 +0530

    Google Play services for Ads. New Adview layout. Crashlytics API key update on Manifest.

--- app/src/main/java/com/njlabs/showjava/MainApplication.java
+++ app/src/main/java/com/njlabs/showjava/MainApplication.java
@@ -1,16 +1,26 @@
 package com.njlabs.showjava;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.crashlytics.android.Crashlytics;
 import com.njlabs.showjava.utils.FontsOverride;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MainApplication extends Application {
 	
     public void onCreate() 
     {
         super.onCreate();
-        Crashlytics.start(this);
-        FontsOverride.setDefaultFont(this);
+        Fabric.with(this, new Crashlytics());
+        FontsOverride.with(this);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 }
