dd341c25c11370a5a4cc7b3a6bf64ad9f264d137
Niranjan Rajendran <niranjan94@yahoo.com>
2015-05-29 22:47:45 +0530

    Fonts override support for API 21+

--- app/src/main/java/com/njlabs/showjava/utils/FontsOverride.java
+++ app/src/main/java/com/njlabs/showjava/utils/FontsOverride.java
@@ -6,28 +6,42 @@ package com.njlabs.showjava.utils;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.os.Build;
+import android.util.Log;
 
 import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 public final class FontsOverride {
 
-    public static void setDefaultFont(Context context) {
+    public static void with(Context context) {
         final Typeface regular = Typeface.createFromAsset(context.getAssets(),"roboto_light.ttf");
         replaceFont("DEFAULT", regular);
-        replaceFont("MONOSPACE", regular);
         replaceFont("SANS_SERIF", regular);
     }
 
     protected static void replaceFont(String staticTypefaceFieldName,  final Typeface newTypeface) {
-        try {
-            final Field staticField = Typeface.class
-                    .getDeclaredField(staticTypefaceFieldName);
-            staticField.setAccessible(true);
-            staticField.set(null, newTypeface);
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Map<String, Typeface> newMap = new HashMap<String, Typeface>();
+            newMap.put("sans-serif", newTypeface);
+            try {
+                final Field staticField = Typeface.class
+                        .getDeclaredField("sSystemFontMap");
+                staticField.setAccessible(true);
+                staticField.set(null, newMap);
+            } catch (Exception e) {
+                Log.e("com.njlabs.showjava",e.toString());
+            }
+        } else {
+            try {
+                final Field staticField = Typeface.class
+                        .getDeclaredField(staticTypefaceFieldName);
+                staticField.setAccessible(true);
+                staticField.set(null, newTypeface);
+            } catch (Exception e) {
+                Log.e("com.njlabs.showjava", e.toString());
+            }
         }
     }
 }
