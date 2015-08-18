ac6855a79afb1249a6b5c086b8238aa03eddbe13
Niranjan Rajendran <niranjan94@yahoo.com>
2015-08-18 15:36:56 +0530

    Added pro and free build flavour

--- app/src/main/java/com/njlabs/showjava/ui/About.java
+++ app/src/main/java/com/njlabs/showjava/ui/About.java
@@ -1,7 +1,9 @@
 package com.njlabs.showjava.ui;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.njlabs.showjava.BuildConfig;
 import com.njlabs.showjava.R;
 
 public class About extends BaseActivity {
@@ -10,5 +12,6 @@ public class About extends BaseActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setupLayout(R.layout.activity_about);
+		((TextView) findViewById(R.id.AppVersion)).setText("Version " + BuildConfig.VERSION_NAME);
 	}
 }
