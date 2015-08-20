3f7c4df64b67bfc4838766b4a4ad7ac17c7d66df
Niranjan Rajendran <niranjan94@yahoo.com>
2015-08-20 21:37:13 +0530

    Added StringUtils for future use

---
+++ app/src/main/java/com/njlabs/showjava/utils/StringUtils.java
@@ -0,0 +1,40 @@
+package com.njlabs.showjava.utils;
+
+@SuppressWarnings("unused")
+public class StringUtils {
+
+    public static String replace(String text, String searchString, String replacement) {
+        return replace(text, searchString, replacement, -1);
+    }
+
+    @SuppressWarnings("StringBufferMayBeStringBuilder")
+    public static String replace(String text, String searchString, String replacement, int max) {
+        if (isEmpty(text) || isEmpty(searchString) || replacement == null || max == 0) {
+            return text;
+        }
+        int start = 0;
+        int end = text.indexOf(searchString, start);
+        if (end == -1) {
+            return text;
+        }
+        int replLength = searchString.length();
+        int increase = replacement.length() - replLength;
+        increase = (increase < 0 ? 0 : increase);
+        increase *= (max < 0 ? 16 : (max > 64 ? 64 : max));
+        StringBuffer buf = new StringBuffer(text.length() + increase);
+        while (end != -1) {
+            buf.append(text.substring(start, end)).append(replacement);
+            start = end + replLength;
+            if (--max == 0) {
+                break;
+            }
+            end = text.indexOf(searchString, start);
+        }
+        buf.append(text.substring(start));
+        return buf.toString();
+    }
+
+    public static boolean isEmpty(String testString) {
+        return "".equals(testString);
+    }
+}
