1ce7712815a0fd4b06b857a521c07ecfdae88212
Niranjan Rajendran <niranjan94@yahoo.com>
2015-12-03 15:55:40 +0530

    Simplify exception handler

--- app/src/main/java/com/njlabs/showjava/utils/ExceptionHandler.java
+++ app/src/main/java/com/njlabs/showjava/utils/ExceptionHandler.java
@@ -2,10 +2,10 @@ package com.njlabs.showjava.utils;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.crashlytics.android.Crashlytics;
 import com.njlabs.showjava.ui.JavaExplorer;
 
 import java.io.PrintWriter;
@@ -30,38 +30,13 @@ public class ExceptionHandler implements java.lang.Thread.UncaughtExceptionHandl
 
     public void uncaughtException(Thread thread, Throwable exception) {
 
+        Crashlytics.logException(exception);
+
         StringWriter stackTrace = new StringWriter();
         exception.printStackTrace(new PrintWriter(stackTrace));
         StringBuilder errorReport = new StringBuilder();
         errorReport.append("************ CAUSE OF ERROR ************\n\n");
         errorReport.append(stackTrace.toString());
-
-        errorReport.append("\n************ DEVICE INFORMATION ***********\n");
-        errorReport.append("Brand: ");
-        errorReport.append(Build.BRAND);
-        errorReport.append(LINE_SEPARATOR);
-        errorReport.append("Device: ");
-        errorReport.append(Build.DEVICE);
-        errorReport.append(LINE_SEPARATOR);
-        errorReport.append("Model: ");
-        errorReport.append(Build.MODEL);
-        errorReport.append(LINE_SEPARATOR);
-        errorReport.append("Id: ");
-        errorReport.append(Build.ID);
-        errorReport.append(LINE_SEPARATOR);
-        errorReport.append("Product: ");
-        errorReport.append(Build.PRODUCT);
-        errorReport.append(LINE_SEPARATOR);
-        errorReport.append("\n************ FIRMWARE ************\n");
-        errorReport.append("SDK: ");
-        errorReport.append(Build.VERSION.SDK_INT);
-        errorReport.append(LINE_SEPARATOR);
-        errorReport.append("Release: ");
-        errorReport.append(Build.VERSION.RELEASE);
-        errorReport.append(LINE_SEPARATOR);
-        errorReport.append("Incremental: ");
-        errorReport.append(Build.VERSION.INCREMENTAL);
-        errorReport.append(LINE_SEPARATOR);
 
         Log.e("com.njlabs.showjava", errorReport.toString());
 
