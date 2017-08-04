66f1abb9e74b937632d1e567e224eadecaeb17df
Niranjan Rajendran <me@niranjan.io>
2017-08-05 00:37:20 +0530

    Add RxBus impl

---
+++ app/src/main/kotlin/com/njlabs/showjava/utils/rx/bus/RxBus.kt
@@ -0,0 +1,30 @@
+package com.njlabs.showjava.utils.rx.bus
+
+import com.jakewharton.rxrelay2.PublishRelay
+import com.jakewharton.rxrelay2.Relay
+
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
+
+class RxBus private constructor() {
+
+    private val _bus = PublishRelay.create<Any>().toSerialized()
+
+    fun send(o: Any) {
+        _bus.accept(o)
+    }
+
+    fun asFlowable(): Flowable<Any> {
+        return _bus.toFlowable(BackpressureStrategy.LATEST)
+    }
+
+    fun hasObservers(): Boolean {
+        return _bus.hasObservers()
+    }
+
+    private object SingletonHolder { val INSTANCE = RxBus() }
+
+    companion object {
+        val instance: RxBus by lazy { SingletonHolder.INSTANCE }
+    }
+}
