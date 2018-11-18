ee6caaeac1a0f2ced71e6a37285d4c045851e719
Niranjan Rajendran <me@niranjan.io>
2018-11-18 18:57:56 +0530

    Add copyright notice to all source files

--- app/src/main/kotlin/com/njlabs/showjava/utils/Notifier.kt
+++ app/src/main/kotlin/com/njlabs/showjava/utils/Notifier.kt
@@ -1,3 +1,21 @@
+/*
+ * Show Java - A java/apk decompiler for android
+ * Copyright (c) 2018 Niranjan Rajendran
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.njlabs.showjava.utils
 
 import android.app.NotificationManager
@@ -14,13 +32,13 @@ class Notifier(
 
     var time: Long = 0
 
-    public fun updateTitle(title: String) {
+    fun updateTitle(title: String) {
         notificationBuilder.setContentTitle(title)
         notificationBuilder.setProgress(0, 0, true)
         notificationManager.notify(notificationTag, notificationId, notificationBuilder.build())
     }
 
-    public fun updateText(text: String) {
+    fun updateText(text: String) {
         val currentTime = System.currentTimeMillis()
         if (currentTime - time >= 500) {
             notificationBuilder.setContentText(text)
@@ -30,7 +48,7 @@ class Notifier(
         }
     }
 
-    public fun updateTitleText(title: String, text: String) {
+    fun updateTitleText(title: String, text: String) {
         notificationBuilder.setContentTitle(title)
         notificationBuilder.setContentText(text)
         notificationBuilder.setProgress(0, 0, true)
